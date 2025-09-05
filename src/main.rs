892ae4ec7c4919b8645d843bcaca3f8cbe0ff4e2
duck-coder404 <76528155+duck-coder404@users.noreply.github.com>
2025-09-05 08:28:49 +0300

    feat: switch to using ping_rs

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,41 @@
-use std::io::{Result, prelude::*};
-use std::net::TcpStream;
+use ping_rs::{self, send_ping};
+use std::net::{IpAddr, Ipv4Addr};
+use std::thread;
+use std::time::{Duration, SystemTime};
 
 fn main() {
     println!("Hello, world!");
-    for _ in 0..3 {
-        let online: bool = ping("google.com").is_ok();
-        println!("{:?}", ping("google.com"));
-        if online {
-            println!("Yes, you are online");
-        } else {
-            println!("No, you are offline");
+
+    // Parse the IP address
+    let addr: IpAddr = IpAddr::V4(Ipv4Addr::new(142, 250, 180, 174));
+    // Prepare ping data
+    let data = [1; 4];
+    // Set timeout
+    let timeout = Duration::from_secs(1);
+    // Configure ping options (optional)
+    let options = ping_rs::PingOptions {
+        ttl: 128,
+        dont_fragment: true,
+    };
+
+    let mut previous_state: Option<bool> = None;
+
+    loop {
+        let current_state = send_ping(&addr, timeout, &data, Some(&options)).is_ok();
+
+        // Check if the state has changed
+        if previous_state != Some(current_state) {
+            if current_state {
+                // Print in green
+                println!("\x1b[32m{:?} Yes, you are online\x1b[0m", SystemTime::now());
+            } else {
+                // Print in red
+                println!("\x1b[31m{:?} No, you are offline\x1b[0m", SystemTime::now());
+            }
+            // Update the previous state
+            previous_state = Some(current_state);
         }
+
+        thread::sleep(Duration::new(1, 0)); // Sleep
     }
-}
-
-fn ping(addr: &str) -> Result<()> {
-    let addr = addr.to_owned() + ":80";
-    let mut stream = TcpStream::connect(addr)?;
-
-    stream.write_all(&[1])?;
-    stream.read_exact(&mut [0; 128])?;
-    Ok(())
 }
